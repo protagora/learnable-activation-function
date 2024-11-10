d275aeee62fe1d900e491ff1fe4fb840e649cae2
Marko <m.milovanovic@gmail.com>
2024-11-10 07:42:42 +0100

    Added SimpleCNN

--- modules/cdflearnableactivation_cuda.py
+++ modules/cdflearnableactivation_cuda.py
@@ -3,6 +3,23 @@ import torch.nn as nn
 from collections import defaultdict
 import datetime
 
+class SimpleCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(32 * 56 * 56, 120)
+        self.fc2 = nn.Linear(120, num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
 class CDFLearnableActivation(nn.Module):
     def __init__(self, normalize_by_area=True, device='cpu'):
         super(CDFLearnableActivation, self).__init__()
